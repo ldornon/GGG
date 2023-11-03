4e96e83764f8b37481778e1cf96ee5e938e79b43
ldornon <lucydornon@gmail.com>
2023-11-03 14:26:34 -0600

    Used random forest for predictions

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -1,5 +1,9 @@
 library(tidymodels)
 library(vroom)
+library(embed)
+library(DataExplorer)
+library(GGally)
+library(discrim)
 
 ggg_train <- vroom("./train.csv") 
 ggg_mv_train <- vroom("./trainWithMissingValues.csv")
@@ -13,4 +17,48 @@ baked <- bake(prep, new_data = ggg_mv_train)
 
 rmse_vec(ggg_train[is.na(ggg_mv_train)],baked[is.na(ggg_mv_train)])
 
+###############
 
+my_mod <- rand_forest(mtry = tune(),
+                      min_n = tune(),
+                      trees = 1000) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
+
+My_Recipe <- recipe(type~., data = ggg_train) %>%
+  step_other(all_nominal_predictors(), threshold = .001)
+prep <- prep(My_Recipe)
+baked <- bake(prep, new_data = ggg_train)
+
+
+GGG_RF_workflow <- workflow() %>% 
+  add_recipe(My_Recipe) %>% 
+  add_model(my_mod)
+
+tuning_grid <- grid_regular(mtry(range= c(1,length(ggg_train)-1)),
+                            min_n(),
+                            levels = 5)
+
+folds <- vfold_cv(ggg_train, v=10, repeats = 1)
+
+CV_results <- GGG_RF_workflow %>% 
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(accuracy))
+
+bestTune <- CV_results %>% 
+  select_best("accuracy")
+
+final_wf <-GGG_RF_workflow  %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = ggg_train)
+
+Random_forest_preds <-final_wf %>% 
+  predict(new_data = ggg_test, type = "class")
+
+Random_preds <-tibble(id =ggg_test$id,
+                      type = Random_forest_preds$.pred_class)
+
+vroom_write(x=Random_preds, file="./GGG_RF_Preds.csv", delim=",")
+
+  
