81ee82043958f161e95047000c6f23dfab9a1425
ldornon <lucydornon@gmail.com>
2023-11-06 15:59:48 -0700

    Fit an MLP in terms of accuracy

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -61,4 +61,51 @@ Random_preds <-tibble(id =ggg_test$id,
 
 vroom_write(x=Random_preds, file="./GGG_RF_Preds.csv", delim=",")
 
+
+#######
+#Neural Networks
+
+nn_recipe <- recipe(type~., data = ggg_train) %>% 
+  update_role(id, new_role = "id") %>% 
+  step_range(all_numeric_predictors(), min=0, max=1)
+
+nn_model<- mlp(hidden_units= tune(),
+               epochs = 50) %>% 
+  set_engine("nnet") %>% 
+  set_mode("classification")
+
+nn_wf <-workflow() %>% 
+  add_recipe(nn_recipe) %>% 
+  add_model(nn_model)
+folds <- vfold_cv(ggg_train, v = 10, repeats = 1)
+
+nn_tuneGrid<- grid_regular(hidden_units(range = c(1, 50)),
+                           levels =10 )
+tuned_nn <- nn_wf %>% 
+  tune_grid(resamples = folds, 
+            grid =nn_tuneGrid, 
+            metrics= metric_set(accuracy))
+tuned_nn %>% collect_metrics() %>% 
+  filter(.metric=="accuracy") %>% 
+  ggplot(aes(x= hidden_units, y= mean)) + geom_line()
+bestTune <- tuned_nn %>% select_best("accuracy")
+
+final_wf <- nn_wf %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = ggg_train)
+NN_preds <-final_wf %>% 
+  predict(new_data = ggg_test, type = "class")
+
+NeuralN_preds <-tibble(id =ggg_test$id,
+                      type = NN_preds$.pred_class)
+
+vroom_write(x=NeuralN_preds, file="./NN_Preds.csv", delim=",")
+
+
+
+
+
+
+
+
   
